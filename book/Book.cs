db97be63c3a2e581fcca4e66904658e522acb0ec
yuhusniyarova <yu.husniyarova@mail.ru>
2012-02-25 10:56:22 +0400

    YARRRR

--- book/Book.cs
+++ book/Book.cs
@@ -5,17 +5,7 @@ using System.Text;
 
 namespace book
 {
-    class Book
-    {
-        public int ISBN { get; set; }
-        public string Name { get; set; }
-        public string Author { get; set; }
-        public Book()
-        { }
 
-        public override string ToString()
-        {
-            return base.ToString();
         }
     }
 }
